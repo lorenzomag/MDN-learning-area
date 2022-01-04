b0aa2a0f4ff3d6f7d42040f8e036fe4417c6ca82
Lorenzo Principe <28869147+lorenzomag@users.noreply.github.com>
2022-01-04 10:00:28 +0100

    Finish _bouncing balls task_

--- javascript/oojs/bouncing-balls/main.js
+++ javascript/oojs/bouncing-balls/main.js
@@ -1,5 +1,7 @@
 // setup canvas
 
+// const BALLS = 25;
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -12,3 +14,82 @@ function random(min, max) {
   const num = Math.floor(Math.random() * (max - min + 1)) + min;
   return num;
 }
+
+
+// Ball object
+function Ball(x, y, velX, velY, color, size) {
+  this.x = x;
+  this.y = y;
+  this.velX = velX;
+  this.velY = velY;
+  this.color = color;
+  this.size = size;
+}
+
+Ball.prototype.draw = function () {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+Ball.prototype.update = function () {
+  if ((this.x + this.size) >= width) {
+    this.velX *= -1;
+  }
+  if ((this.x - this.size) <= 0) {
+    this.velX *= -1;
+  }
+  if ((this.y + this.size) >= height) {
+    this.velY *= -1;
+  }
+  if ((this.y - this.size) <= 0) {
+    this.velY *= -1;
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+let balls = [];
+
+while (balls.length < 25) {
+  let size = random(10, 20);
+  let ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`,
+    size
+  );
+  // console.log(balls.length);
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = 'rgba(0,0,0,0.5)';
+  ctx.fillRect(0, 0, width, height);
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+    balls[i].collisionDetect();
+  }
+  requestAnimationFrame(loop);
+}
+
+Ball.prototype.collisionDetect = function(){
+  for (let j=0; j< balls.length; j++){
+    if (!(this === balls[j])){
+      const dx= this.x - balls[j].x;
+      const dy= this.y - balls[j].y;
+      const distance = Math.sqrt(dx**2 + dy**2);
+
+      if(distance < this.size+balls[j].size){
+        balls[j].color = this.color = `rgb(${random(0,255)},${random(0,255)},${random(0,255)})`;
+      }
+    }
+  }
+}
+
+loop();
